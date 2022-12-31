4e394d5b08bdd433964ffb1d203075101e0fdde1
Moaz <moaz.eldefrawy@gmail.com>
2022-12-31 14:28:30 +0200

    parser simulator added

--- src/Simulator/Simulator.cpp
+++ src/Simulator/Simulator.cpp
@@ -32,7 +32,7 @@ void Simulator::consumeCharacter(char character) {
 void Simulator::flushLastAcceptingState() {
   if (lastAcceptingState_) {
     // flush last accepting state and consume succeeding characters
-    o_ << lastAcceptingState_->getAcceptValue().value().value << '\n';
+    tokens.push_back(lastAcceptingState_->getAcceptValue().value().value);
     lastAcceptingState_.reset();
     currentState_ = startState_;
     std::vector<char> newInput = std::move(buffer);
@@ -64,5 +64,79 @@ void Simulator::finishSimulation() {
 }
 
 std::vector<std::string> Simulator::getErrors() {
-  return errors;
+  return vector<string>(errors);
+}
+
+std::vector<std::string> Simulator::getTokens() {
+  return vector<string>(tokens);
+}
+
+ParserSimulator::ParserSimulator(map<pair<NT,string>,ProductionContent> table, ProductionToken topNT) {
+  tokenStack.push_back({END_INPUT_TOKEN, true});
+  tokenStack.push_back(topNT);
+}
+
+void ParserSimulator::consumeToken(string token) {
+  NT nt = tokenStack.back().first;
+  
+  history.push_back(tokenStack);
+  printTokens(tokenStack); cout << '\t';
+
+  if(isTerminalToken(tokenStack.back())) {
+    if(token == nt){
+      tokenStack.pop_back();
+    } else {
+      // TODO: handle error
+      string err = "Error: missing token \"" + token + "\", inserted.";
+      cout << err << endl;;
+      errors.push_back(err);
+      tokenStack.pop_back();
+    }
+  } else {
+    if(table.find({nt, token}) != table.end()) {
+      ProductionContent pc = table[{nt, token}];
+
+      if(isٍٍٍSyncProduction(pc)) {
+        tokenStack.pop_back();
+        cout <<"sync production\n";
+      } 
+
+      else {
+        reverse(pc.begin(), pc.end());
+        tokenStack.pop_back();
+        tokenStack.insert(tokenStack.end(), pc.begin() , pc.end());
+
+        reverse(pc.begin(), pc.end());
+        printProduction(nt, pc);
+        cout << '\n';  
+      }
+
+      consumeToken(token);
+    } else {
+      //TODO: handle error
+      string err = "Error:(illegal \""+ nt +"\") – discard " + token;
+      cout << err << endl;
+      errors.push_back(err);
+
+    }
+  }
+}
+
+bool ParserSimulator::isTerminalToken(ProductionToken pt){
+  return pt.second;
+}
+
+bool ParserSimulator::isٍٍٍSyncProduction(ProductionContent pt) {
+  return pt.size() == 1 && pt.back().first == "#" && pt.back().second;
+}
+
+void ParserSimulator::printTokens(vector<ProductionToken> v){ 
+  for(ProductionToken pt: v) {
+    cout<< pt.first;
+  }
+}
+
+void ParserSimulator::printProduction(NT nt, ProductionContent pc){ 
+  cout << nt <<"-->"; 
+  printTokens(pc);
 }
--- src/Simulator/Simulator.h
+++ src/Simulator/Simulator.h
@@ -3,11 +3,16 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <map>
+#include <vector>
+#include <string>
+#include "CFGParser/CFGParser.h"
+
+using namespace std;
 
 class Simulator {
  public:
-  explicit Simulator(std::shared_ptr<const dfa::State> startState,
-                     std::ostream& o = std::cout);
+  explicit Simulator(std::shared_ptr<const dfa::State> startState);
   Simulator() = delete;
 
   void consumeCharacter(char character);
@@ -15,6 +20,7 @@ class Simulator {
   void finishSimulation();
 
   std::vector<std::string> getErrors();
+  std::vector<std::string> getTokens();
  private:
 
   void flushLastAcceptingState();
@@ -24,5 +30,33 @@ class Simulator {
   std::ostream& o_;
   std::shared_ptr<const dfa::State> lastAcceptingState_;
   std::vector<char> buffer;
+  std::vector<std::string> errors;
+  std::vector<string> tokens;
+};
+
+
+using NT = string;  // Non-Terminal Token
+
+const string END_INPUT_TOKEN = "$";
+const string SYNC_TOKEN = "#";
+
+class ParserSimulator {
+ public:
+  explicit ParserSimulator(map<pair<NT,string>,ProductionContent> table, ProductionToken topNT);
+  ParserSimulator() = delete;
+
+  void consumeToken(string token);
+
+  void finishSimulation();
+
+  vector<string> getErrors();
+ private:
+  void printProduction(NT nt, ProductionContent pc);
+  void printTokens(vector<ProductionToken> v);
+  bool isٍٍٍSyncProduction(ProductionContent pc);
+  bool isTerminalToken(ProductionToken pt);
+  
+  vector<vector<ProductionToken>> history;
+  vector<ProductionToken> tokenStack;
   std::vector<std::string> errors;
 };
