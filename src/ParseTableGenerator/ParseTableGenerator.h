33981dde03c6c38126e11cbd9a1cd8a8333a2d60
ABE-Mark45 <ahmedinmasr@gmail.com>
2023-01-03 00:52:14 +0200

    fix code formatting

--- src/ParseTableGenerator/ParseTableGenerator.h
+++ src/ParseTableGenerator/ParseTableGenerator.h
@@ -1,17 +1,25 @@
-#include <string>
 #include <map>
-#include <vector>
+#include <string>
 #include <utility>
+#include <vector>
 using namespace std;
 
 class ParseTableGenerator {
-public:
-    static map<pair<string/*NT*/, string/*token*/>, vector<pair<string/*NT or Terminal*/, bool>>>
-    getTable(const map<string, vector<vector<pair<string, bool>>>> &,const string& start_symbol);
+ public:
+  static map<pair<string /*NT*/, string /*token*/>,
+             vector<pair<string /*NT or Terminal*/, bool>>>
+  getTable(const map<string, vector<vector<pair<string, bool>>>>&,
+           const string& start_symbol);
 
-    //this string means state , every state maps to more than one production
-    //every production it maps to is either true or false
-    static void getFirst(const map<string, vector<vector<pair<string, bool>>>> &m, map<basic_string<char>, vector<pair<basic_string<char>, vector<pair<basic_string<char>, bool>>>>> &first);
-    static void getFollow(const map<string, vector<vector<pair<string, bool>>>> &m, map<string, vector<string>> &follow,const string& start_symbol);
-    static bool get_is_ambiguous();
+  //this string means state , every state maps to more than one production
+  //every production it maps to is either true or false
+  static void getFirst(
+      const map<string, vector<vector<pair<string, bool>>>>& m,
+      map<basic_string<char>,
+          vector<pair<basic_string<char>,
+                      vector<pair<basic_string<char>, bool>>>>>& first);
+  static void getFollow(
+      const map<string, vector<vector<pair<string, bool>>>>& m,
+      map<string, vector<string>>& follow, const string& start_symbol);
+  static bool get_is_ambiguous();
 };
--- tests/LL1GeneratorTest/LL1GeneratorTest.cpp
+++ tests/LL1GeneratorTest/LL1GeneratorTest.cpp
@@ -6,64 +6,35 @@
 TEST(LL1GeneratorTest, BigTest) {
 
   ProductionsTable inputTable{
-      {"S", {{{"A", false}, {"a", true}},{{"b", true}}}},
+      {"S", {{{"A", false}, {"a", true}}, {{"b", true}}}},
       {"A",
-       {
-           {{"A", false}, {"c", true}},
-           {{"S", false}, {"d", true}},
-           {{"l", true}}
-       }}};
+       {{{"A", false}, {"c", true}},
+        {{"S", false}, {"d", true}},
+        {{"l", true}}}}};
 
   // The map orders the non-terminals by alphabetic order
   const ProductionsTable outputTable{
-    {"S", 
-    {
-        {{"l", true}, {"A'", false}, {"a", true}, {"S'", false}},
-        {{"b", true}, {"S'", false}}
-    }},
-    {"S'",
-    {
-        {{"d", true}, {"A'", false}, {"a", true}, {"S'", false}},
-        {}
-    }},
-    {"A",
-    {
-        {{"S", false}, {"d", true}, {"A'", false}},
-        {{"l", true}, {"A'", false}}
-    }},
-    {"A'",
-    {
-        {{"c", true}, {"A'", false}},
-        {}
-    }}
-  };
+      {"S",
+       {{{"l", true}, {"A'", false}, {"a", true}, {"S'", false}},
+        {{"b", true}, {"S'", false}}}},
+      {"S'", {{{"d", true}, {"A'", false}, {"a", true}, {"S'", false}}, {}}},
+      {"A",
+       {{{"S", false}, {"d", true}, {"A'", false}},
+        {{"l", true}, {"A'", false}}}},
+      {"A'", {{{"c", true}, {"A'", false}}, {}}}};
 
   LL1Generator ll1Generator(inputTable);
 
   ASSERT_EQ(outputTable, ll1Generator.getProductionsTable());
 }
 
-
 TEST(LL1GeneratorTest, SmallTest) {
 
-  ProductionsTable inputTable{
-      {"A",
-       {
-           {{"A", false}},
-           {{"b", true}}
-       }}};
+  ProductionsTable inputTable{{"A", {{{"A", false}}, {{"b", true}}}}};
 
   // Epsilon productions are empty vectors
-  const ProductionsTable outputTable{
-    {"A", 
-    {
-        {{"b", true}, {"A'", false}}
-    }},
-    {"A'",
-    {
-        {}
-    }}
-  };
+  const ProductionsTable outputTable{{"A", {{{"b", true}, {"A'", false}}}},
+                                     {"A'", {{}}}};
 
   LL1Generator ll1Generator(inputTable);
 
@@ -74,25 +45,13 @@ TEST(LL1GeneratorTest, FactoringSmall) {
 
   ProductionsTable inputTable{
       {"A",
-       {
-           {{"X", false}, {"Y", false}, {"Z", false}, {"c", true}},
-           {{"X", false}, {"Y", false}, {"Z", false}, {"b", true}}
-       }
-      }
-    };
+       {{{"X", false}, {"Y", false}, {"Z", false}, {"c", true}},
+        {{"X", false}, {"Y", false}, {"Z", false}, {"b", true}}}}};
 
   // Epsilon productions are empty vectors
   const ProductionsTable outputTable{
-    {"A", 
-    {
-        {{"X", false}, {"Y", false}, {"Z", false}, {"A#0", false}}
-    }},
-    {"A#0",
-    {
-        {{"b", true}},
-        {{"c", true}}
-    }}
-  };
+      {"A", {{{"X", false}, {"Y", false}, {"Z", false}, {"A#0", false}}}},
+      {"A#0", {{{"b", true}}, {{"c", true}}}}};
 
   LL1Generator ll1Generator(inputTable);
 
@@ -103,45 +62,18 @@ TEST(LL1GeneratorTest, FactoringBig) {
 
   ProductionsTable inputTable{
       {"A",
-       {
-           {{"X", false}, {"Y", false}, {"f", true}},
-           {{"X", false}, {"Y", false}, {"g", true}},
-           {{"X", false}, {"Y", false}, {"Z", false}, {"c", true}},
-           {{"X", false}, {"Y", false}, {"Z", false}, {"b", true}}
-       }
-      },
-      {"B",
-       {
-        {{"b", true}}
-       }
-      }
-    };
+       {{{"X", false}, {"Y", false}, {"f", true}},
+        {{"X", false}, {"Y", false}, {"g", true}},
+        {{"X", false}, {"Y", false}, {"Z", false}, {"c", true}},
+        {{"X", false}, {"Y", false}, {"Z", false}, {"b", true}}}},
+      {"B", {{{"b", true}}}}};
 
   // Epsilon productions are empty vectors
   const ProductionsTable outputTable{
-    {"A", 
-     {
-        {{"X", false}, {"Y", false}, {"A#1", false}}
-     }},
-    {"A#0",
-     {
-        {{"b", true}},
-        {{"c", true}}
-     }
-    },
-    {"A#1",
-    {
-        {{"Z", false}, {"A#0", false}},
-        {{"f", true}},
-        {{"g", true}}
-    }
-    },
-    {"B",
-       {
-        {{"b", true}}
-       }
-    }
-  };
+      {"A", {{{"X", false}, {"Y", false}, {"A#1", false}}}},
+      {"A#0", {{{"b", true}}, {{"c", true}}}},
+      {"A#1", {{{"Z", false}, {"A#0", false}}, {{"f", true}}, {{"g", true}}}},
+      {"B", {{{"b", true}}}}};
 
   LL1Generator ll1Generator(inputTable);
 
@@ -150,46 +82,31 @@ TEST(LL1GeneratorTest, FactoringBig) {
 
 TEST(LL1GeneratorTest, AlreadyLL1) {
 
-  ProductionsTable inputTable{
-      {"A",
-       {
-        {{"a", true}, {"B", false}}
-       }
-      },
-      {"B",
-       {
-        {{"b", true}, {"C", false}, {"b", true}}//,
-        //{}
-       }
-      },
-      {"C",
-       {
-        {{"c", false}}//,
-        //{}
-       }
-      }
-    };
+  ProductionsTable inputTable{{"A", {{{"a", true}, {"B", false}}}},
+                              {"B",
+                               {
+                                   {{"b", true}, {"C", false}, {"b", true}}  //,
+                                   //{}
+                               }},
+                              {"C",
+                               {
+                                   {{"c", false}}  //,
+                                                   //{}
+                               }}};
 
   // Epsilon productions are empty vectors
   const ProductionsTable outputTable{
-      {"A",
-       {
-        {{"a", true}, {"B", false}}
-       }
-      },
+      {"A", {{{"a", true}, {"B", false}}}},
       {"B",
        {
-        {{"b", true}, {"C", false}, {"b", true}}//,
-        //{}
-       }
-      },
+           {{"b", true}, {"C", false}, {"b", true}}  //,
+                                                     //{}
+       }},
       {"C",
        {
-        {{"c", false}}//,
-        //{}
-       }
-      }
-    };
+           {{"c", false}}  //,
+                           //{}
+       }}};
 
   LL1Generator ll1Generator(inputTable);
 
@@ -199,61 +116,22 @@ TEST(LL1GeneratorTest, AlreadyLL1) {
 TEST(LL1GeneratorTest, RemoveEpsilon) {
 
   ProductionsTable inputTable{
-      {"A",
-       {
-        {{"a", true}, {"B", false}}
-       }
-      },
-      {"B",
-       {
-        {{"b", true}, {"C", false}, {"b", true}},
-        {}
-       }
-      },
-      {"C",
-       {
-        {{"c", false}},
-        {}
-       }
-      }
-    };
+      {"A", {{{"a", true}, {"B", false}}}},
+      {"B", {{{"b", true}, {"C", false}, {"b", true}}, {}}},
+      {"C", {{{"c", false}}, {}}}};
 
   // Epsilon productions are empty vectors
   const ProductionsTable outputTable{
-      {"A",
-       {
-        {{"a", true}, {"A#0", false}}
-       }
-      },
-      {"A#0",
-        {
-          {},
-          {{"B", false}}
-        }
-      },
-      {"B",
-       {
-        {{"b", true}, {"B#0", false}}
-       }
-      },
-      {"B#0",
-        {
-          {{"C", false}, {"b", true}},
-          {{"b", true}}
-        }
-      },
-      {"C",
-       {
-        {{"c", false}}
-       }
-      }
-    };
+      {"A", {{{"a", true}, {"A#0", false}}}},
+      {"A#0", {{}, {{"B", false}}}},
+      {"B", {{{"b", true}, {"B#0", false}}}},
+      {"B#0", {{{"C", false}, {"b", true}}, {{"b", true}}}},
+      {"C", {{{"c", false}}}}};
 
   LL1Generator ll1Generator(inputTable);
 
   ASSERT_EQ(outputTable, ll1Generator.getProductionsTable());
 }
-
 
 /*
 UGHHHH I need to remove redundant productions.. 
